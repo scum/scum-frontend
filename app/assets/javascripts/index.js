9c335dc0751ea886e43c08add01d594dd7b61894
Gabriele Cirulli <gabrycir@gmail.com>
2015-05-13 16:44:28 +0200

    Get flux going

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,10 @@
 // Application entry point
 document.addEventListener("DOMContentLoaded", function () {
   document.documentElement.classList.remove("loading");
+
+  // TODO: Initialize stores
+
+  // TODO: Initialize socket.io action dispatcher
+
+  // TODO: Start react-router
 });
